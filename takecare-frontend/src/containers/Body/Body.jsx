8017f8493cfcc7ef257cb4f57862278eb6a57596
Manel <quemonmanel@gmail.com>
2023-01-04 22:06:04 +0100

    feat:body.jsx+some chorework:files

--- takecare-frontend/src/containers/Body/Body.jsx
+++ takecare-frontend/src/containers/Body/Body.jsx
@@ -2,4 +2,18 @@
         import React from "react"; 
         import "./Body.css"; 
         import {Route,Routes} from "react-router-dom";
-        
+        import Home from "../Home/Home";
+        import Login from "../Login/Login";
+        import Register from "../Register/Register"; 
+
+        const Body = () => {
+            return (
+                <div className="classbody">
+                    <Routes>
+                        <Route path = "/" element={<Home/>}/>
+                        <Route path = "/login" element={<Login/>}/>
+                        <Route path = "/register" element={<Register/>}/>
+                    </Routes>
+                </div>
+            )
+        }
--- takecare-frontend/src/containers/Header/Header.jsx
+++ takecare-frontend/src/containers/Header/Header.jsx
@@ -7,13 +7,15 @@
             return (
                 <div className="headerDesign">
                     <div>
-                        <Link to "./login">
+                        <Link to="./login">
                           <button>Login</button>
                         </Link>
-                        <Link to "./register">
+                        <Link to="./register">
                           <button>Register</button>
                         </Link>    
                     </div>
                 </div>
             );
         };
+
+        export default Header; 
