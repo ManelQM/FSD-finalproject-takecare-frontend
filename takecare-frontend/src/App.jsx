58852d3900073fa1d38199c3a088d2bd66de6de4
Manel <quemonmanel@gmail.com>
2023-01-09 09:32:12 +0100

    feat:body.jsx route connecting containers

--- takecare-frontend/src/App.jsx
+++ takecare-frontend/src/App.jsx
@@ -1,18 +1,21 @@
 
     import Header from "./containers/Header/Header";
     import Body from "./containers/Body/Body";
+    import Footer from "./containers/Footer/Footer";
 
 
 
     function App() {
     
       return (
-        <>
+        <>   
           <Header/>
           <Body/>
+          <Footer />
         </>
       )
     }
 
-    export default App
+    export default App;
 
+    
