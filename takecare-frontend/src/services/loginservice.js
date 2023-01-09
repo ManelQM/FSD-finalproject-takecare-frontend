fd17929981523a6ed3cd7a0b3ae8abfd0688f17a
Manel <quemonmanel@gmail.com>
2023-01-09 16:50:24 +0100

    feat:loginservices + axios+react jwt

---
+++ takecare-frontend/src/services/loginservice.js
@@ -0,0 +1,11 @@
+import axios from 'axios'; 
+
+const API_URL = 'http://localhost:3001';
+
+let returnedMessage = ""; 
+
+export let loginUser = async (values) => {
+    try {
+        await axios.post(`${API_URL}`)
+    }
+}
