86cd1b8c4826fd66e07e0032b25d29977c7acd82
Manel <quemonmanel@gmail.com>
2023-01-12 23:15:21 +0100

    feat:userarea cards

--- takecare-frontend/src/containers/UserArea/UserArea.jsx
+++ takecare-frontend/src/containers/UserArea/UserArea.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 import './UserArea.css';
+import {Col,Card,Row,Button} from 'react-bootstrap'
 
 const UserArea = () => {
     return (
-        <div className="userAreaDesign">
-            
+        
+        
 
-        </div>
+<Row xs={1} md={5} className=" cardDesign g-4 d-flex justify-content-center align-items-center column" >
+      {Array.from({ length:4}).map((_, idx) => (
+        <Col>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160"/>
+            <Card.Body>
+              <Card.Title>Profile Services Contracts Publications</Card.Title>
+              <Card.Text >
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text >
+              <Button className="butonDesign"></Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+
+       
     )
 }
 
