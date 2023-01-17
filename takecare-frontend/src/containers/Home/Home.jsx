a3dd1bdccab08c952cb80ab2c52fc0f7213f99b2
Manel <quemonmanel@gmail.com>
2023-01-17 17:45:08 +0100

    feat:search bar and button plus starting hooks and handlers

--- takecare-frontend/src/containers/Home/Home.jsx
+++ takecare-frontend/src/containers/Home/Home.jsx
@@ -1,30 +1,43 @@
 import "./Home.css"; 
 import React from "react";
-import { Container,Row,Col } from "react-bootstrap";
+import { Container,Row,Col,Form, FormControl, Button} from "react-bootstrap";
 
 
 
 const Home =() => {
     return ( 
+        
     <div className="homeDesign">
         
         <Container style={{justifyContent:"flex-end"}}>
         <Row  style={{height:"7em",justifyContent:"flex-start"}}>
-        <Col md={12} className="textDesing" 
+        
+        <Col md={6} className="textDesing"
         style={{
         fontFamily: 'Fredoka One',
         textAlign:"center",
-        fontSize: "2em"
-        }} >Welcome to TakeCare</Col>
-        <Col md={12} className="searchDesign rowDesign">
-            the navbar
+        fontSize: "4em",
+        }} >Welcome to TakeCare
+        </Col>
+       
+        <Col md={12} placeholder="search" className="searchDesign rowDesign"
+        style={{alignItems:"center", justifyContent:"center",textAlign:"center"}}
+        >
+             <Form>
+            <FormControl
+              type="text"
+              placeholder="Search..."
+            />
+            <Button style={{marginTop:"1em"}} type="submit">Search</Button>
+            </Form>
         </Col>
       </Row>
         </Container>
-    
     </div>
     )
 }
+
+
 
 
 
