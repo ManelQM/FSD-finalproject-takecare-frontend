b5f264d148b6c3f6d5e6d1ec4e6b7fab21268927
Manel <quemonmanel@gmail.com>
2023-01-25 21:53:16 +0100

    feat: profile date working with redux

--- takecare-frontend/src/containers/Login/Login.jsx
+++ takecare-frontend/src/containers/Login/Login.jsx
@@ -1,143 +1,148 @@
-import React, { useState } from 'react';
-import "./Login.css"; 
+import React, { useState } from "react";
+import "./Login.css";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { login } from "./loginSlice";
-import { Button, Form, Input, } from "antd";
-import { Container } from 'react-bootstrap';
+import { Button, Form, Input } from "antd";
+import { Container } from "react-bootstrap";
 import { decodeToken } from "react-jwt";
-import { loginUser } from '../../services/apiCalls';
-
-
+import { loginUser } from "../../services/apiCalls";
 
 const Login = () => {
+  const [messageText, setMessageText] = useState({ message: "" });
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const onFinish = async (values) => {
+    let res = await loginUser(values);
+    if (res == "Invalid E-mail or password.") {
+      setMessageText({
+        message: res,
+      });
+    } else {
+      let decoded = decodeToken(res.jwt);
+
+      let idrolenavigate = decoded.user.idrole;
+
+      let userCredentials = {
+        token: res,
+        user: decoded.user,
+      };
+
+      dispatch(login(userCredentials));
+
+      if (idrolenavigate == 2) {
+        navigate("../userarea");
+      } else if (idrolenavigate == 1) {
+        navigate("../adminarea");
+      } else {
+        setMessageText({
+          message: "This is weird",
+        });
+      }
+    }
+  };
+
+  const onFinishFailed = (errorInfo) => {};
+
+  return (
+    <div className="loginDesign" style={{alignItems:"center", justifyContent:"center"}}>
         
-    const [messageText, setMessageText] = useState({ message: "" });
-
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-
-    const onFinish = async values => {
-            let res = await loginUser(values);
-            if (res == "Invalid E-mail or password.") {
-            setMessageText({
-                message: res,
-            });
-            } else {
-                
-            let decoded = decodeToken(res.jwt);
-
-            let idrolenavigate = decoded.user.idrole
-            
-            let userCredentials = {
-                token: res, 
-                user: decoded.user 
-            }
-
-            dispatch(login(userCredentials));
-
-            if (idrolenavigate == 2) {
-                navigate("../userarea");
-            } else if (idrolenavigate == 1 ) {
-                navigate("../adminarea");
-            } else {
-                setMessageText({
-                message: "This is weird",
-                });
-            }
-            }
-
-    };
-
-    const onFinishFailed = errorInfo => {};
+      <Form
         
-    return (
-        <div className="loginDesign">
-            
-            <Form
-                name="basic"
-                style={{
-                width: "20em",
-                height: "20em",
-                marginTop: "5em",
-                marginRight: "5em",
-            }}
-            labelCol={{
-            span: 8,
-            }}
-            wrapperCol={{
-            span: 16,
-            }}
-            initialValues={{
-            remember: true,
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
+        name="basic"
+        style={{
+        //   width: "20em",
+        //   height: "20em",
+        //   alignItems:"center",
+        //   justifyContent: "center"
+        //   marginTop: "5em",
+        //   marginRight: "5em",
+        marginLeft:"-3em"
+        }}
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          name="email"
+          label="E-mail"
+          rules={[
+            {
+              type: "email",
+              className: "email",
+              message: (
+                <div style={{ color: "black", fontFamily: "Fredoka One" }}>
+                  The input is not valid E-mail!
+                </div>
+              ),
+            },
+            {
+              required: true,
+              message: (
+                <div style={{ color: "black", fontFamily: "Fredoka One" }}>
+                  Please input your E-mail!
+                </div>
+              ),
+            },
+          ]}
         >
-            <Form.Item
-            name="email"
-            label="E-mail"
-            rules={[
-                {
-                type: "email",
-                className: "email",
-                message: (
-                    <div style={{ color: "black",fontFamily:"Fredoka One" }}>
-                    The input is not valid E-mail!
-                    </div>
-                ),
-                },
-                {
-                required: true,
-                message: (
-                    <div style={{ color: "black",fontFamily:"Fredoka One" }}>Please input your E-mail!</div>
-                ),
-                },
-            ]}
-            >
-            <Input />
-            </Form.Item>
-            <Form.Item
-            
-            label="Password"
-            name="password"
-            rules={[
-                {
-                required: true,
-                message: (
-                    <div style={{ color: "black", fontFamily:"Fredoka One" }}>
-                    Please input your password!
-                    </div>
-                ),
-                },
-            ]}
-            >
-            <Input.Password />
-            </Form.Item>
-            <Form.Item
-            wrapperCol={{
-                offset: 8,
-                span: 16,
-            }}
-            >
-            <Button type="primary" className='animeButton' htmlType="submit"
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: (
+                <div style={{ color: "black", fontFamily: "Fredoka One" }}>
+                  Please input your password!
+                </div>
+              ),
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button
+            type="primary"
+            className="animeButton"
+            htmlType="submit"
             style={{
-                backgroundColor:"white",
-                color:"black",
-                fontFamily:"Fredoka One",
-                borderRadius:50,
-                marginTop:"1em",
-                borderColor:"black"}}>
-                Submit
-            </Button>
-            </Form.Item>
-        </Form>
-        
-        <div className="messageText">{messageText.message}</div>
-        
-            </div>   
-    )
+              backgroundColor: "white",
+              color: "black",
+              fontFamily: "Fredoka One",
+              borderRadius: 50,
+              marginTop: "1em",
+              borderColor: "black",
+            }}
+          >
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+
+
+      <div className="messageText">{messageText.message}</div>
+    </div>
+  );
 };
 
-export default Login; 
+export default Login;
--- takecare-frontend/src/containers/Profile/Profile.jsx
+++ takecare-frontend/src/containers/Profile/Profile.jsx
@@ -1,57 +1,61 @@
-import React from "react";
-import "./Profile.css"; 
-import colorhearthand from  '../../assets/newicons/handscolorwithheart.png';
-import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBTypography, MDBIcon } from 'mdb-react-ui-kit';
-import { useNavigate } from 'react-router-dom';
-import {useState, useEffect} from "react"
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import {getProfile} from "../../services/apiCalls";
-import {userData} from "../Profile/profileSlice";
+import React, { useState, useEffect } from "react";
+import "./Profile.css";
+import colorhearthand from "../../assets/newicons/handscolorwithheart.png";
+import { useNavigate } from "react-router-dom";
 
+import { useSelector, useDispatch } from "react-redux";
 
+import { getProfile } from "../../services/apiCalls";
+import { userData } from "../Profile/profileSlice";
+import Card from 'react-bootstrap/Card';
 const Profile = () => {
-    
-        const [userProfile, setUserProfile] = useState([]);
-        const navigate = useNavigate()
-        const user = useSelector(userData);
-        const dispatch = useDispatch();
-        const [error, setError] = useState('');
-        const userJWT = JSON.parse(localStorage.getItem("SAVEJWT"))
-    
-        useEffect(() => {
-           
-    
-            if (userProfile.length === 0) {
-                console.log (userProfile, "hola!")
-                    getProfile(userJWT)
-                    .then(
-                        (res) => {
-                            console.log(res.userProfile)
-                            setUserProfile(res.data)
-                           
-                        }
-                    )
-                    .catch((error) => {
-                        setError(error.response?.data)
-                    })
-            };
-    
-    
-        }, [userProfile]);
-    
-    
-   
-      return (
-            <div className="profileDesign">
+  const navigate = useNavigate(); 
 
+  const userReduxCredentials = useSelector(userData);
+  const dispatch = useDispatch(); 
+
+if (userReduxCredentials?.token !== "" ) { 
   
-
-            </div>
-        )
-    }
-
-    export default Profile; 
-
-
+  return (  
     
+         <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>{userReduxCredentials?.user?.name}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">{userReduxCredentials?.user?.nickname}Card Subtitle</Card.Subtitle>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+       
+      </Card.Body>
+    </Card>
+
+  );
+}
+}
+export default Profile;
+
+
+//   const [userProfile, setUserProfile] = useState([]);
+//   const navigate = useNavigate();
+//   const user = useSelector(userData);
+//   const dispatch = useDispatch();
+//   const [error, setError] = useState("");
+//   const userJWT = JSON.parse(localStorage.getItem("SAVEJWT"));
+
+//   useEffect(() => {
+//     if (userProfile.length === 0) {
+//       console.log(userProfile, "hola!");
+//       getProfile(userJWT)
+//         .then((res) => {
+//           console.log(res.userProfile);
+//           setUserProfile(res.data);
+//         })
+//         .catch((error) => {
+//           setError(error.response?.data);
+//         });
+//     }
+//   }, [userProfile]);
