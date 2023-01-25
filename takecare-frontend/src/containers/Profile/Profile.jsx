029fa5293f7844ccd5a682d886d53e0c6ed13dda
Manel <quemonmanel@gmail.com>
2023-01-25 19:16:08 +0100

    chore: lets start with a good get profile

--- takecare-frontend/src/containers/Profile/Profile.jsx
+++ takecare-frontend/src/containers/Profile/Profile.jsx
@@ -45,61 +45,6 @@ const Profile = () => {
       return (
             <div className="profileDesign">
 
-    <section  style={{ backgroundColor: '#f4f5f7' }}>
-      <MDBContainer fluid className="py-5 h-100">
-        <MDBRow  className="justify-content-center align-items-center h-100">
-          <MDBCol  lg="" className="mb-4 mb-lg-0">
-            <MDBCard className="cardDesign animeButton" style={{ borderRadius: '.5rem',width:"20em",height:"25em" }}>
-              <MDBRow className="g-0">
-                <MDBCol md="4" className="gradient-custom text-center text-white"
-                  style={{ borderTopLeftRadius: '.5rem', borderBottomLeftRadius: '.5rem' }}>
-                  <MDBCardImage variant="top" src={colorhearthand}
-                    alt="Avatar" className="my-5" style={{ width: '80px' }} fluid />
-                  <MDBTypography tag="h5"></MDBTypography>
-                  <MDBCardText></MDBCardText>
-                  <MDBIcon far icon="edit mb-5" />
-                </MDBCol>
-                <MDBCol md="8">
-                  <MDBCardBody className="p-4">
-                    <MDBTypography tag="h6">Information</MDBTypography>
-                    <hr className="mt-0 mb-4" />
-                    <MDBRow className="pt-1">
-                      <MDBCol size="6" className="mb-3">
-                        <MDBTypography tag="h6">Name and Surname</MDBTypography>
-                        <MDBCardText className="text-muted">{userProfile.name} {userProfile.surname}</MDBCardText>
-                      </MDBCol>
-                      <MDBCol size="6" className="mb-3">
-                        <MDBTypography tag="h6">Nickname</MDBTypography>
-                        <MDBCardText className="text-muted">{userProfile.nickname}</MDBCardText>
-                      </MDBCol>
-                    </MDBRow>
-
-                    <MDBTypography tag="h6"></MDBTypography>
-                    <hr className="mt-0 mb-4" />
-                    <MDBRow className="pt-1">
-                      <MDBCol size="6" className="mb-3">
-                        <MDBTypography tag="h6">Email</MDBTypography>
-                        <MDBCardText className="text-muted">{userProfile.email}</MDBCardText>
-                      </MDBCol>
-                      <MDBCol size="6" className="mb-3">
-                        <MDBTypography tag="h6">City</MDBTypography>
-                        <MDBCardText className="text-muted">{userProfile.city}</MDBCardText>
-                      </MDBCol>
-                    </MDBRow>
-
-                    <div className="d-flex justify-content-start">
-                      <a href="#!"><MDBIcon fab icon="facebook me-3" size="lg" /></a>
-                      <a href="#!"><MDBIcon fab icon="twitter me-3" size="lg" /></a>
-                      <a href="#!"><MDBIcon fab icon="instagram me-3" size="lg" /></a>
-                    </div>
-                  </MDBCardBody>
-                </MDBCol>
-              </MDBRow>
-            </MDBCard>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
-    </section>
   
 
             </div>
