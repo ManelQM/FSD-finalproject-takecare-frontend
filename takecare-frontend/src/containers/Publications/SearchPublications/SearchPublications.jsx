48e247a6898aa08ea054e2505648b84180a43ce4
Manel <quemonmanel@gmail.com>
2023-03-16 13:32:36 +0100

    feat: debounce bringing data

--- takecare-frontend/src/containers/Publications/SearchPublications/SearchPublications.jsx
+++ takecare-frontend/src/containers/Publications/SearchPublications/SearchPublications.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 import {publicationsReq} from "../../../services/apiCalls";
-
+import {useDispatch, useSelector} from "react-redux"; 
+import {addSearch, cleanSearch,searchPublicationData} from "./searchPublicationsSlice";
 import "./SearchPublications.css"; 
 
 const SearchPublications = () => {
+    const [criteria, setCriteria] = useState("");
+    const dispatch = useDispatch();
+    const {publications} = useSelector(searchPublicationData);
+
+    const inputHandler = (e) => {
+        setCriteria(e.targe.value);
+    };
+
+    useEffect (() => {
+        if (criteria === "") {
+            dispatch(cleanSearch({ publications: [] }));
+        } else {
+            const debounce = setTimeout (() => {
+                publicationsReq(criteria)
+                .then((res) => {
+                    dispatch (addSearch ({
+                        publications: res.data.results
+                    }));
+                })
+                .catch((error) => console.log(error));
+            }, 350);
+            return () => clearTiemout(debounce);
+        }
+    }, [criteria, dispatch]);
 
     return (
         <div className="searchPublicationsDesign">
